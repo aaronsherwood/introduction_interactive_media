a1cd905f8f1aa16e4924e50d0c22369479ccf4d8
aaron sherwood <aaronsherwood@aarons-MacBook-Pro.local>
2018-01-31 17:49:59 +0400

    maintain state example

---
+++ arduinoExamples/buttonMaintainState/buttonMaintainState.ino
@@ -0,0 +1,31 @@
+//set global variables
+int ledPin = 2;
+int button = 7;
+bool onOff = false;
+bool prevButtonState = LOW;
+
+void setup() {
+  // set pinmodes
+  pinMode(ledPin, OUTPUT);
+  pinMode(button, INPUT);
+}
+
+void loop() {
+  //read our button & store it in a boolean variable
+  bool currButtonState = digitalRead(button);
+
+  //if the button is pressed AND it wasn't pressed last frame
+  //then flip onOff to its opposite
+  //we want to make sure we don't keep flipping on & off while the button is pressed
+  //checking the previous time let's us only flip it once
+  if (currButtonState == HIGH && prevButtonState == LOW) {
+    onOff = !onOff;
+  }
+
+  //turn our LED on & off based on our global onOff state
+  digitalWrite(ledPin, onOff);
+
+  //set the previous button state to our current state,
+  //so next time through loop it will actually be the previous state
+  prevButtonState = currButtonState;
+}
