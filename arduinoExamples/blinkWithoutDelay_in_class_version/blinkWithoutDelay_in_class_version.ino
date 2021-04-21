df4eb38b4437991e294134b75c9d2ffd76f0f6fd
aaronsherwood <info@aaron-sherwood.com>
2021-04-21 13:09:58 +0400

    blink no delay

--- arduinoExamples/blinkWithoutDelay_in_class_version/blinkWithoutDelay_in_class_version.ino
+++ arduinoExamples/blinkWithoutDelay_in_class_version/blinkWithoutDelay_in_class_version.ino
@@ -1,46 +1,18 @@
-//set global variables
-int ledPin = 2;
-int button = 3;
+const int ledPin = 6;
+long timer = 0;
+int timerLength = 500;
 bool onOff = false;
-bool prevButtonState = LOW;
-int blinkRate = 20000;
-bool isLEDon = LOW;
-unsigned long nextBlinkTime = 0;
-
-
+ 
 void setup() {
-  // set pinmodes
+  // put your setup code here, to run once:
   pinMode(ledPin, OUTPUT);
-  pinMode(button, INPUT);
-
-  Serial.begin(9600);
 }
-
+ 
 void loop() {
-  bool currButtonState = digitalRead(button);
-
-  if (currButtonState == HIGH && prevButtonState == LOW) {
+  // put your main code here, to run repeatedly:
+  if (millis()>timer){
     onOff = !onOff;
+    digitalWrite(ledPin, onOff);
+    timer = millis() + timerLength;
   }
-
-  //get the current time
-  unsigned long currentTime = millis();
-
-  Serial.print("Current time: ");
-  Serial.print(currentTime);
-  Serial.print(" Next Blink Time: ");
-  Serial.println(nextBlinkTime);
-  
-  if (currentTime > nextBlinkTime){
-    isLEDon = !isLEDon;
-    nextBlinkTime = currentTime + blinkRate;
-  }
-
-    if (onOff){
-      digitalWrite(ledPin, isLEDon);
-    } else {
-      digitalWrite(ledPin, LOW);
-    }
-  
-  prevButtonState = currButtonState;
 }
