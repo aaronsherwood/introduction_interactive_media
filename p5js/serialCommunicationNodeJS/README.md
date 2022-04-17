ac488fa6947960718367cb57b95518922da3e601
aaronsherwood <info@aaron-sherwood.com>
2022-04-17 11:21:08 +0400

    node serial p5js

---
+++ p5js/serialCommunicationNodeJS/README.md
@@ -0,0 +1,10 @@
+# Node OSC Serial
+
+## Usage
+
+* Download and install [Node.js](https://nodejs.org/) if you don't have it already.
+* On the command line, navigate to the folder that contains all the files for this example.
+* Install Node Modules with `npm install` (only need to do this once)
+* In *server.js* set the serial port name.
+* Start the server: `node server.js`
+* Navigate to localhost:8000 for the p5js sketch
---
+++ p5js/serialCommunicationNodeJS/sketch.js
@@ -0,0 +1,34 @@
+let xPos = 0;
+let yPos = 0;
+let onOff = "0";
+
+function setup() {
+  createCanvas(640, 480);
+  noStroke();
+
+  // Serail call and response throuhg a websocket
+  socket.on('message', function(msg) {
+      if (msg != null) {
+        var list = split(trim(msg), ",");
+        print(list);
+        if (list.length == 2) {
+          xPos = map(list[0], 0, 1023, 0, width); // element 0 is the locH
+          yPos = map(list[1], 550, 250, 0, height); // element 1 is the locV
+        }
+      }
+      socket.emit('message', onOff);
+    });
+}
+
+function draw() {
+  background(200);
+  ellipse(xPos, yPos, 50, 50); // draw the circle
+  if (mouseIsPressed) {
+    if (mouseX <= width / 2)
+      onOff = "1";
+    else
+      onOff = "2";
+  } else {
+    onOff = "0";
+  }
+}
